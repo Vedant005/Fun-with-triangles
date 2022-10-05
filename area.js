ef5ee41f79c763bf0bcb3b7b6f9adafa5fc86317
Vedant005 <vedantkanekar909@gmail.com>
2022-10-05 13:31:10 +0530

    fun with triangles
    completed

--- area.js
+++ area.js
@@ -11,3 +11,27 @@ function calculateArea (base,height){
 
 
 areaButton.addEventListener("click", caluclateArea);
+
+
+
+// function productOfSides(a, b) {
+//   const sideProduct = a * b;
+//   return sideProduct;
+// }
+
+function calculateArea() {
+  var base = Number(sides[0].value);
+  var height = Number(sides[1].value);
+
+  if (base > 0 && height > 0) {
+    const sideProduct = productOfSides(base, height);
+    const area = 1 / 2 * (sideProduct);
+    message.innerText = `The area of triangle with the entered values is ${area} Cm-Square`;
+
+  } else {
+    message.innerText = `Input Error: Please Enter valid values in all the fields!`;
+
+  }
+}
+
+
