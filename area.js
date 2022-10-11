1779960656012a42b1488e7038a408b9f4c5dbda
Vedant005 <vedantkanekar909@gmail.com>
2022-10-11 22:05:16 +0530

    changed css and added footer

--- area.js
+++ area.js
@@ -1,37 +1,84 @@
-const sides = documeny.querySelectorAll(".side-input");
-const areaButton = document.querySelector("#area-btn");
+const sides = document.querySelectorAll(".side-input");
+const areaBtn = document.querySelector("#area-btn");
 const output = document.querySelector("#output");
 
+function calculateProductOfSides(a,b){
+  const productOfSides = a*b;
+  return productOfSides;
 
-function calculateArea (base,height){
-    const productArea= 1/2* base*height;
-    return area;
+}
+
+function calculateArea(){
+  const productOfSides = calculateProductOfSides(Number(sides[0].value), Number(sides[1].value));
+  const valueOfArea = (0.5)*(productOfSides);
+  output.innerText = "Area of triangle is " + valueOfArea.toFixed(2);
+}
+
+areaBtn.addEventListener("click", calculateArea)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  function calculateArea (base,height){
+//     const area= 1/2* base*height;
+//     return area;
     
-}
+//  }
 
 
-areaButton.addEventListener("click", caluclateArea);
+// areaBtn.addEventListener("click", calculateArea);
 
 
 
-// function productOfSides(a, b) {
-//   const sideProduct = a * b;
-//   return sideProduct;
-// }
+// //  function productOfSides(a,b) {
+// //   const sideProduct = a * b;
+// //  return sideProduct;
+// // }
 
-function calculateArea() {
-  var base = Number(sides[0].value);
-  var height = Number(sides[1].value);
+// function calculateArea() {
+//   // if (base > 0 && height > 0){
+//     const area = calculateArea(Number(sides[0].value), Number(sides[1].value));
 
-  if (base > 0 && height > 0) {
-    const sideProduct = productOfSides(base, height);
-    const area = 1 / 2 * (sideProduct);
-    message.innerText = `The area of triangle with the entered values is ${area} Cm-Square`;
-
-  } else {
-    message.innerText = `Input Error: Please Enter valid values in all the fields!`;
-
-  }
-}
+    
+//       output.innerText = `The area of triangle with the entered values is ${area} Cm-Square`;
+//   }
+//     // else {
+//     //  output.innerText = `Input Error: Please Enter valid values in all the fields!`;
+  
+//     // }
+  
+  
+ 
 
 
--- hypotenuse.js
+++ hypotenuse.js
@@ -3,16 +3,16 @@ const hypotenuseButton = document.querySelector("#hypotenuse-btn");
 const outputE1 = document.querySelector("#output");
 
 
-function caluclateSumOfSquares(a, b) {
+function calculateSumOfSquares(a, b) {
     const sumOfSquares = a*a + b*b;
      return sumOfSquares;
 }
 
 
-function caluclateHypotenuse() {
-    const sumOfSquares = caluclateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
+function calculateHypotenuse() {
+    const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value));
     const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
     outputE1.innerText = "The length of hypotenuse is " + lengthOfHypotenuse.toFixed(2);
 }
 
-hypotenuseButton.addEventListener("click", caluclateHypotenuse);
+hypotenuseButton.addEventListener("click", calculateHypotenuse);
